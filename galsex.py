96042786c4861c7ac11349e69e1f3de1239b168e
Andrew Crooks <andrewtcrooks@gmail.com>
2014-07-25 01:00:26 -0700

    Revamped code to produce galaxies on small postage stamps instead of large images

--- galsex.py
+++ galsex.py
@@ -18,66 +18,108 @@ Runtime ~ 8 hr  10 min
 
 import os
 import sys
-import glob                                 # Find files
+import pyfits
 
 #Dashboard
-#n = 10 # number of simulated galaxies to create per input image
-sexdir = '/opt/local/bin/'                  # sextractor dir
-galtab = 'galmodel.tab'
-wdir = os.getcwd()+'/'
-
-
-# Local Subdirectories
-sexconfig = 'sexconfig/'
-simcomb = 'simcomb/'
-simgalset = 'simgalset/'
-simcat = 'simcat/'
-sexcat = 'sexcat/'
-
-
-# Prefixes for output files
-galcomb = 'galcomb'                                   # image with n sim gal added
-galmodelset = 'galmodelset'
-sim = 'sim'
-sex = 'sex'         #
-
-
-# Set number of itertions, sextractor config file, zeropoint magnitude, and verbosity
-iteration = [1000, 10]
-defaultsex = 'default.sex'
-defaultsim = 'defaultsim.sex'
-zp = 25.9463                                # zeropoint magnitude
+iteration = [5, 2000]
 output = sys.stdout
 verbose = 1
+sexdir = '/opt/local/bin/'                  # sextractor dir
+
+# Prefixes for iterated input and output files
+galcomb = 'galcomb'                                   # image with n sim gal added
+galmodel = 'galmodel'                                 # sim galaxy file prefix
+galmodelset = 'galmodelset'
+sim = 'sim'
+sex = 'sex'
+
+# Field and Filter selector
+y = 0       # Field index
+z = 0       # Filter index
+
+fields = ['uds', 'bootes']
+filters = ['f160w', 'f125w', 'f814w', 'f606w', 'f350lp']
+zeropoint = [25.96, 26.25, 25.94333, 26.49113, 26.94]
+psf_fwhm = [0.18, 0.12, 0.09, 0.08, 0.08]              # PSF FWHM in units of arcseconds
+field = fields[y]
+filt = filters[z]
+zp = zeropoint[z]
+psf = psf_fwhm[z]
+
+
+# Local Subdirectories and special input files
+wdir = os.getcwd()+'/'
+science = wdir+'science/'+field+'/'+filt+'/'
+simcomb = science+'simcomb/'
+simgal = science+'simgal/'
+sexconfig = science+'sexconfig/'
+simgalset = science+'simgalset/'
+simcat = science+'simcat/'
+sexcat = science+'sexcat/'
+
+galtab = 'galmodel_'+field+'_'+filt+'.tab'
+
+
+# Instrument selector
+if filt is 'f160w' or filters is 'f125w':
+    inst = 'wfc3'
+
+elif filt is 'f814w' or filters is 'f606w':
+    inst = 'acs'
+
+elif filt is 'f350lp':
+    inst = 'uvis'
+
+else:
+    sys.exit('Error: No Filter Selected!')
+
+
+# Sextractor config file parameters
+defaultsex = 'default.sex'
+defaultsim = 'default.sex'
+weight_type = 'MAP_RMS'
+weight_image = science+'hlsp_candels_hst_'+inst+'_'+field+'-tot_'+filt+'_v1.0_rms.fits'
 
 
 # Scan directory for input fits files while excluding program files and alert if no .fits input images are present
-images = glob.glob('*.fits')
+# Read in image data and header from science image
+image = pyfits.open(science+'hlsp_candels_hst_'+inst+'_'+field+'-tot_'+filt+'_v1.0_drz.fits')
+hdu = image[0]
+data = hdu.data
+hdr = hdu.header
+ccdgain = hdr['CCDGAIN']
+exptime = hdr['EXPTIME']
 
-l = len(images)
 
+for x in xrange(iteration[0]):
 
-# Loop through science image versions
-for w in xrange(l):
+    # Run sextractor in single image mode on new image (galcomb*.fits)
+    cmnd = sexdir + 'sex ' + simcomb + galcomb + str(x) + '.fits'
+    cmnd += ' -c ' + sexconfig + defaultsex
+    cmnd += ' -catalog_name ' + sexcat + sex + str(x) + '.cat'
+    cmnd += ' -GAIN ' + str(ccdgain * exptime)
+    cmnd += ' -MAG_ZEROPOINT ' + str(zp)
+    cmnd += ' -SEEING_FWHM ' + str(psf)
+    cmnd += ' -WEIGHT_TYPE ' + weight_type
+    cmnd += ' -WEIGHT_IMAGE ' + weight_image
+    os.system(cmnd)
 
-    # Loop through galcomb_w_x.fits images
-    for x in xrange(iteration[0]):
+    if verbose:
+        output.write('Generated sextractor catalog: ' + sex+str(x)+'.cat \n')
 
-        # Run sextractor in single image mode on new image (galcomb*.fits)
-        cmnd = sexdir+'sex '+wdir+simcomb+galcomb+'_'+str(w)+'_'+str(x)+'.fits -c '+sexconfig+defaultsex
-        cmnd += ' -mag_zeropoint '+str(zp)+' -catalog_name '
-        cmnd += wdir+sexcat+sex+'_'+str(w)+'_'+str(x)+'.cat'
-        os.system(cmnd)
+    # Run sextractor in dual image mode:
+    #   Find mag of galmodelset*.fits image (used for aperture correction)
+    #   using the same apertures from the corresponding galcomb*.fits image
+    cmnd2 = sexdir + 'sex ' + simcomb + galcomb + str(x) + '.fits,'
+    cmnd2 += simgalset + galmodelset + str(x) + '.fits'
+    cmnd2 += ' -c ' + sexconfig + defaultsex
+    cmnd2 += ' -catalog_name ' + simcat + sim + str(x) + '.cat'
+    cmnd2 += ' -GAIN ' + str(ccdgain * exptime)
+    cmnd2 += ' -MAG_ZEROPOINT ' + str(zp)
+    cmnd2 += ' -SEEING_FWHM ' + str(psf)
+    cmnd2 += ' -WEIGHT_TYPE ' + weight_type
+    cmnd2 += ' -WEIGHT_IMAGE ' + weight_image
+    os.system(cmnd2)
 
-        # Run sextractor in dual image mode:
-        #   Find mag of galmodelset*.fits image (used for aperture correction)
-        #   using the same apertures from the corresponding galcomb*.fits image
-        cmnd2 = sexdir+'sex '+wdir+simcomb+galcomb+'_'+str(w)+'_'+str(x)+'.fits,'
-        cmnd2 += wdir+simgalset+galmodelset+str(x)+'.fits -c '+sexconfig+defaultsim
-        cmnd2 += ' -mag_zeropoint '+str(zp)+' -catalog_name '
-        cmnd2 += wdir+simcat+sim+'_'+str(w)+'_'+str(x)+'.cat'
-        os.system(cmnd2)
-
-        if verbose:
-            output.write('Generated sextractor catalog: '
-                         + sex+'_'+str(w)+'_'+str(x)+'.cat \n')
+    if verbose:
+        output.write('Generated sextractor catalog: ' + sim+str(x)+'.cat \n')
